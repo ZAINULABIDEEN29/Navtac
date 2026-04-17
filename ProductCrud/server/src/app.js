2e62ec60ca7acc5bcaa7c73dd1410bb02914a893
Zain ul Abideen <zk438256@gmail.com>
2026-04-17 16:58:55 +0500

    fixed cors issue

--- ProductCrud/server/src/app.js
+++ ProductCrud/server/src/app.js
@@ -14,7 +14,7 @@ connectDB();
 
 app.use(cors({
    origin:process.env.FRONTEND_URL,
-   credentials:true
+  
 }));
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
